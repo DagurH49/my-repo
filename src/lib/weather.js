18a5c0bb551db9435514daf02154481f1eff934e
DagurH49 <Dagurhr@gmail.com>
2024-10-30 11:39:54 +0000

    laga heimskar breytingar

--- src/lib/weather.js
+++ src/lib/weather.js
@@ -1,10 +1,6 @@
 const API_URL = 'https://api.open-meteo.com/v1/forecast';
 
-/**
- * Sleep for a given number of milliseconds.
- * @param {number} ms - Milliseconds to sleep.
- * @returns {Promise} - Promise that resolves after the given time.
- */
+
 async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -21,7 +17,9 @@ async function sleep(ms) {
  * @param {any} data Gögn frá Open Meteo.
  * @returns {Array<Forecast>}
  */
+
 function parseResponse(data) {
+  console.log(data);
   const hourly = data.hourly;
   const { time = [], precipitation = [], temperature_2m = [] } = hourly;
 
@@ -56,10 +54,8 @@ function parseResponse(data) {
  * @returns {Promise<Array>} - Skilar loforði sem leysist upp í veðurspá.
  */
 export async function weatherSearch(latitude, longitude) {
-  await sleep(1000); // Simulate network delay
 
-  const url = `${API_URL}?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation&timezone=GMT&forecast_days=1`;
-
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,apparent_temperature,precipitation&timezone=GMT&forecast_days=1`;
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Gat ekki sótt veðurspá');
@@ -67,8 +63,13 @@ export async function weatherSearch(latitude, longitude) {
 
   const data = await response.json();
 
-  // Breyta gögnunum í það form sem við viljum
-  const results = parseResponse(data);
+  const results = data.hourly.time.map((time, index) => ({
+    time,
+    temperature: data.hourly.temperature_2m[index],
+    apparentTemperature: data.hourly.apparent_temperature[index],
+    precipitation: data.hourly.precipitation[index],
+  }));
+
 
   return results;
 }
